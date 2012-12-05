5780544f1a0ee67acd09bca5a0b96ab19356b828
Bajee11 <bajee11@gmail.com>
2012-12-05 17:46:54 -0500

    d2att: Include d2att to 4.2.1

---
+++ products/d2att.mk
@@ -0,0 +1,20 @@
+# Inherit AOSP device configuration for d2att.
+$(call inherit-product, device/samsung/d2att/full_d2att.mk)
+
+# Inherit AOKP common bits
+$(call inherit-product, vendor/aokp/configs/common.mk)
+
+# Inherit GSM common stuff.
+$(call inherit-product, vendor/aokp/configs/gsm.mk)
+
+# Setup device specific product configuration.
+PRODUCT_NAME := aokp_d2att
+PRODUCT_BRAND := Samsung
+PRODUCT_DEVICE := d2att
+PRODUCT_MODEL := SGH-I747
+PRODUCT_MANUFACTURER := Samsung
+
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=d2att TARGET_DEVICE=d2att BUILD_FINGERPRINT="d2att-user 4.1.2 JZO54K I747UCALEM release-keys" PRIVATE_BUILD_DESC="samsung/d2att/d2att:4.1.2/JZO54K/I747UCALEM:user/release-keys"
+
+PRODUCT_COPY_FILES += \
+    vendor/aokp/prebuilt/bootanimation/bootanimation_720_1280.zip:system/media/bootanimation.zip
