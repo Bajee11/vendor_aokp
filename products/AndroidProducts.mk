e4fe000653c8fa47c4c8d305c7576eb150a7110d
DAGr8 <renocustom@gmail.com>
2012-12-25 23:09:46 -0500

    bring  i317 to aokp
    Change-Id: Ia4e09da1a31ce96e64ba61086dabc6640cc99bea

--- products/AndroidProducts.mk
+++ products/AndroidProducts.mk
@@ -3,6 +3,7 @@ PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/grouper.mk \
     $(LOCAL_DIR)/i9100.mk \
     $(LOCAL_DIR)/maguro.mk \
+    $(LOCAL_DIR)/t0att.mk \
     $(LOCAL_DIR)/mako.mk \
     $(LOCAL_DIR)/manta.mk \
     $(LOCAL_DIR)/n7000.mk \
